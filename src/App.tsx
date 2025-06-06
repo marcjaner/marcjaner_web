dd44c88a61b32cbbf38a38cdc923ef477b9d7517
marcjaner <marcjanerferrer@gmail.com>
2025-06-06 11:44:58 +0200

    add buy me a coffe widget

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import NotFound from "./pages/NotFound";
 import TagsPage from "./pages/TagsPage";
 import PosthogPolicyPage from "./pages/PosthogPolicyPage";
 import { HelmetProvider } from "react-helmet-async";
+import { useEffect } from "react";
 
 // Create a client with better defaults for our use case
 const queryClient = new QueryClient({
@@ -28,32 +29,52 @@ const queryClient = new QueryClient({
   },
 });
 
-const App = () => (
-  <HelmetProvider>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <BrowserRouter>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/projects" element={<ProjectsPage />} />
-              <Route path="/projects/:slug" element={<ProjectDetail />} />
-              <Route path="/blog" element={<BlogPage />} />
-              <Route path="/blog/:slug" element={<BlogDetail />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="/newsletter" element={<NewsletterPage />} />
-              <Route path="/blog/tags/:tag" element={<TagsPage />} />
-              <Route path="/posthog/policy" element={<PosthogPolicyPage />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Layout>
-          <Toaster />
-          <Sonner />
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </HelmetProvider>
-);
+const App = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.setAttribute("data-name", "BMC-Widget");
+    script.setAttribute("data-cfasync", "false");
+    script.src = "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js";
+    script.setAttribute("data-id", "marcjaner");
+    script.setAttribute("data-description", "Support me on Buy me a coffee!");
+    script.setAttribute("data-message", "");
+    script.setAttribute("data-color", "#40DCA5");
+    script.setAttribute("data-position", "Right");
+    script.setAttribute("data-x_margin", "18");
+    script.setAttribute("data-y_margin", "18");
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <BrowserRouter>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/projects" element={<ProjectsPage />} />
+                <Route path="/projects/:slug" element={<ProjectDetail />} />
+                <Route path="/blog" element={<BlogPage />} />
+                <Route path="/blog/:slug" element={<BlogDetail />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/newsletter" element={<NewsletterPage />} />
+                <Route path="/blog/tags/:tag" element={<TagsPage />} />
+                <Route path="/posthog/policy" element={<PosthogPolicyPage />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Layout>
+            <Toaster />
+            <Sonner />
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
+  );
+};
 
 export default App;
