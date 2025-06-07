75fbb3029936fe248d1d574f13793ab05dbcadd5
marcjaner <marcjanerferrer@gmail.com>
2025-06-07 12:11:28 +0200

    redirect to buy me a coffe

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -9,16 +8,27 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      "/support": {
+        target: "https://buymeacoffee.com/marcjaner",
+        changeOrigin: true,
+        secure: false,
+        rewrite: () => "",
+        configure: (proxy, _options) => {
+          proxy.on("proxyRes", (proxyRes, _req, _res) => {
+            proxyRes.statusCode = 301;
+          });
+        },
+      },
+    },
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  assetsInclude: ['**/*.md'],
+  assetsInclude: ["**/*.md"],
 }));
